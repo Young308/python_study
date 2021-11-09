5bbc683a0699b022e648ec72715619952669e09f
yoonyoung208 <yoonyoung208@gmail.com>
2021-11-09 12:29:39 +0900

    데이터 변수 문자열

---
+++ 기본데이터타임.py
@@ -0,0 +1,29 @@
+# 기본데이터 타입
+
+a = 7
+b = 7.7777
+c = True
+d = False
+e = '문자열'
+
+type(a)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+# 파이썬은 나눈 결과값이 소수점이면 자동으로 형변화가 일어난다.
+print(2/4)
+print(type(2/4))
+
+# 제곱 , 몫, 나머지
+
+print(7**4)  # 7에 4승
+print(9//4)  # 몫은 2
+print(9 % 4)  # 나머지 1
+
+# 반올림 (숫자 , 표시되는 숫자 자리수 )
+
+print(round(3.141592, 2))  # = 3.14
---
+++ 변수문자열.py
@@ -0,0 +1,34 @@
+# 여러개의 변수를 동시에 초기화
+a, b, c = 1, 2, 3
+
+print(a)
+print(b)
+print(c)
+
+# 예제 2~5
+
+p = 10
+print(p)
+
+x, y, z = 1, 2, 3
+
+print(x)
+print(y)
+print(z)
+
+d = 12
+e = 1.5
+f = 'string'
+
+
+print(type(d))
+print(type(e))
+print(type(f))
+
+a = input('a : ')
+b = input('b : ')
+
+a, b = b, a
+
+print('a : ' + a)
+print('b : ' + b)
